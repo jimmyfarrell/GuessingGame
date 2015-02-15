66d05854ad8b55ec9036ce10200d6093bbe30a62
Jimmy Farrell <jfarrell22@gmail.com>
2015-02-15 00:28:30 -0500

    Full functionality (minus winning screen), plus some style changes

--- guess.js
+++ guess.js
@@ -1,100 +1,134 @@
 var guessCount = 5;
 var answer;
 var playerGuess;
+var previousGuess;
 var allGuesses = [];
+var guessList = document.getElementById("guess-list");
 var guessesLeft = document.getElementById("guesses-left");
-var statusText = document.getElementById("status");
+var statusText = document.getElementById("current-status");
 var hintText = document.getElementById("hint");
+
+function isValidNum() {
+	var valid = true;
+	playerGuess = document.getElementById("player-guess").value;
+	document.getElementById("player-guess").value = "";
+	for (var i = 0; i < allGuesses.length; i++) {
+		if (playerGuess == allGuesses[i]) {
+			valid = false;
+			statusText.innerHTML = "You already guessed that number.";
+		}
+	}
+	if (isNaN(playerGuess)) {
+		valid = false
+		statusText.innerHTML = "Not a number. Please guess again.";
+	}
+	if (valid) {
+		startGame();
+	}
+}
+
+function startGame() {
+	guessCount--;
+	if (guessCount == 4) {
+		answer = getRandomNum();
+	}
+	if (playerGuess == answer) {
+		$("#guess-list").prepend("<li><span style='font-size: 18px; color: #3a2006;'>Guess #" + (5 - guessCount) + ": </span><span style='color: #257a7c;'>" + playerGuess + "</span></li>");
+		statusText.innerHTML = "You won!";
+	}
+	else {
+		$("#guess-list").prepend("<li><span style='font-size: 18px; color: #3a2006;'>Guess #" + (5 - guessCount) + ": </span>" + playerGuess + "</li>");
+		if (guessCount > 0) {
+			allGuesses.push(playerGuess);
+			evaluateGuess();
+		}
+		else {
+			gameLost();
+		}
+	}
+}
 
 function getRandomNum() {
 	var randomNum = Math.floor((Math.random() * 100) + 1);
 	return randomNum;
 }
 
+function evaluateGuess() {
+	if (guessCount == 4) {
+		statusText.innerHTML = howClose() + " " + higherOrLower();
+	}
+	else {
+		statusText.innerHTML = hotOrCold() + " " + higherOrLower();
+	}
+	updateGuessList();
+}
+
+function hotOrCold() {
+	previousGuess = allGuesses[allGuesses.length - 2];
+	var hotCold = "";
+	if (Math.abs(answer - playerGuess) < Math.abs(answer - previousGuess)) {
+		hotCold = "hotter —";
+	}
+	else {
+		hotCold = "colder —";
+	}
+	return hotCold;
+}
+
+function updateGuessList() {
+	if (guessCount === 0) {
+		guessesLeft.innerHTML = "This is your last guess...";
+	}
+	else {
+		guessesLeft.innerHTML = "Guesses left: " + guessCount;
+	}
+}
+
 function howClose() {
-	var distance = ""
+	var firstHint = "";
 	if (Math.abs(answer - playerGuess) <= 5) {
-		distance = "so so so close";
+		firstHint = "So HOT —";
 	}
 	else if (Math.abs(answer - playerGuess) <= 10) {
-		distance = "pretty close";
+		firstHint = "Pretty hot —";
 	}
 	else if (Math.abs(answer - playerGuess) <= 20) {
-		distance = "not very close";
+		firstHint = "Cold —";
 	}
 	else if (Math.abs(answer - playerGuess) > 20) {
-		distance = "far away";
+		firstHint = "Ice cold —";
 	}
-	return distance;
+	return firstHint;
 }
 
 function higherOrLower() {
-	statusText.style.display = "block"
+	var highLow = "";
 	if (answer > playerGuess) {
-		statusText.innerHTML = "You're " + howClose() + ".<br>Try guessing higher.";
+		highLow += "Guess higher";
 	}
 	else {
-		statusText.innerHTML = "You're " + howClose() + ".<br>Try guessing lower.";
+		highLow += "Guess lower";
 	}
+	return highLow;
 }
 
-function gameOver() {
-	statusText.innerHTML = "You lost! :(";
-}
-
-function compareNums() {
-	if (playerGuess == answer) {
-		statusText.innerHTML = "You won!";
-	}
-	else {
-		if (guessCount > 0) {
-			if (guessCount === 0) {
-				statusText.innerHTML = "This is your last guess...";
-			}
-			statusText.innerHTML = guessCount + " guesses left!";
-			allGuesses.push(playerGuess);
-			higherOrLower();
-		}
-		else {
-			gameOver();
-		}
-	}
-}
-
-function startGame() {
-	answer = getRandomNum();
-	compareNums();
-}
-
-function isValidNum() {
-	playerGuess = parseInt(document.getElementById("playerGuess").value);
-	statusText.style.display = "block";
-	hintText.innerHTML = "";
-	document.getElementById("playerGuess").value = '';
-	if (isNaN(playerGuess)) {
-		statusText.innerHTML = "Not a number. Please guess again.";
-	}
-	else if (guessCount == 5) {
-		guessCount--;
-		startGame();
-	}
-	else {
-		guessCount--;
-		compareNums();
-	}
+function gameLost() {
+	statusText.innerHTML = "Oh no! You lost... :(";
 }
 
 function startOver() {
 	guessCount = 5;
-	allGuess = [];
-	statusText.innerHTML = "Remember, you only have 5 guesses.";
+	allGuesses = [];
+	$("ul").empty();
+	statusText.innerHTML = "It's between 1 and 100";
+	guessesLeft.innerHTML = "Guesses left: " + guessCount;
 }
 
 function giveHint() {
 	if (guessCount == 5) {
-		hintText.innerHTML = "Nuh uh, you have to at least try to guess.";
+		statusText.innerHTML = "At least try to guess!";
 	}
 	else {
-		hintText.innerHTML = answer;
+		statusText.innerHTML = "The answer is " + answer;
 	}
 }
